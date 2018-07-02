7b12f88cdbf57744f5759bdcb97e0835ca4a454e
Albert Fougy <albert@fougy.com>
2018-07-02 09:31:33 -0400

    fixed index.js

--- server.js
+++ server.js
@@ -44,3 +44,14 @@ app.listen(PORT, function () {
 }).on('error', err => {
   console.error(err);
 });
+
+// Listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app; // Export for testing
